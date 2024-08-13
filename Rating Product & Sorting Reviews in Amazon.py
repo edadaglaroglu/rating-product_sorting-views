66f63e59fb701ef8d4c4d6c85cb7a8c16ab4be01
Eda Dağlaroğlu <163730599+edadaglaroglu@users.noreply.github.com>
2024-08-13 20:52:48 +0300

    Rating Product & Sorting Reviews in Amazon.py

--- Rating Product & Sorting Reviews in Amazon.py
+++ Rating Product & Sorting Reviews in Amazon.py
@@ -3,25 +3,7 @@
 # PROJE: Rating Product & Sorting Reviews in Amazon
 ###################################################
 
-###################################################
-# İş Problemi
-###################################################
 
-# E-ticaretteki en önemli problemlerden bir tanesi ürünlere satış sonrası verilen puanların doğru şekilde hesaplanmasıdır.
-# Bu problemin çözümü e-ticaret sitesi için daha fazla müşteri memnuniyeti sağlamak, satıcılar için ürünün öne çıkması ve satın
-# alanlar için sorunsuz bir alışveriş deneyimi demektir. Bir diğer problem ise ürünlere verilen yorumların doğru bir şekilde sıralanması
-# olarak karşımıza çıkmaktadır. Yanıltıcı yorumların öne çıkması ürünün satışını doğrudan etkileyeceğinden dolayı hem maddi kayıp
-# hem de müşteri kaybına neden olacaktır. Bu 2 temel problemin çözümünde e-ticaret sitesi ve satıcılar satışlarını arttırırken müşteriler
-# ise satın alma yolculuğunu sorunsuz olarak tamamlayacaktır.
-
-###################################################
-# Veri Seti Hikayesi
-###################################################
-
-# Amazon ürün verilerini içeren bu veri seti ürün kategorileri ile çeşitli metadataları içermektedir.
-# Elektronik kategorisindeki en fazla yorum alan ürünün kullanıcı puanları ve yorumları vardır.
-
-# Değişkenler:
 # reviewerID - ID of the reviewer, e.g. A2SUAM1J3GNN3B
 # asin - ID of the product, e.g. 0000013714
 # reviewerName - name of the reviewer
@@ -46,27 +28,11 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.expand_frame_repr', False)
 pd.set_option('display.float_format', lambda x: '%.5f' % x)
 
-###################################################
-# GÖREV 1: Average Rating'i Güncel Yorumlara Göre Hesaplayınız ve Var Olan Average Rating ile Kıyaslayınız.
-###################################################
-
-# Paylaşılan veri setinde kullanıcılar bir ürüne puanlar vermiş ve yorumlar yapmıştır.
-# Bu görevde amacımız verilen puanları tarihe göre ağırlıklandırarak değerlendirmek.
-# İlk ortalama puan ile elde edilecek tarihe göre ağırlıklı puanın karşılaştırılması gerekmektedir.
-
-
-###################################################
-# Adım 1: Veri Setini Okutunuz ve Ürünün Ortalama Puanını Hesaplayınız.
-###################################################
-
 df = pd.read_csv("datasets/amazon_review.csv")
 df["overall"].mean()
 
 
 ###################################################
-# Adım 2: Tarihe Göre Ağırlıklı Puan Ortalamasını Hesaplayınız.
-###################################################
-
 
 
 # zaman bazlı ortalama ağırlıkların belirlenmesi
@@ -77,32 +43,12 @@ def time_based_weighted_average(dataframe, w1=50, w2=25, w3=15, w4=10):
            dataframe.loc[(dataframe["day_diff"] > dataframe["day_diff"].quantile(0.75)), "overall"].mean() * w4 / 100
 
 
-# Adım 3: Ağırlıklandırılmış puanlamada her bir zaman diliminin ortalamasını karşılaştırıp yorumlayınız.
 
-
-
-###################################################
-# Görev 2: Ürün için Ürün Detay Sayfasında Görüntülenecek 20 Review'i Belirleyiniz.
-###################################################
-
-
-###################################################
-# Adım 1. helpful_no Değişkenini Üretiniz
-###################################################
-
-# Not:
-# total_vote bir yoruma verilen toplam up-down sayısıdır.
-# up, helpful demektir.
-# veri setinde helpful_no değişkeni yoktur, var olan değişkenler üzerinden üretilmesi gerekmektedir.
 
 
 df["helpful_no"] = df["total_vote"] - df["helpful_yes"]
 
 df = df[["reviewerName", "overall", "summary", "helpful_yes", "helpful_no", "total_vote", "reviewTime"]]
-
-###################################################
-# Adım 2. score_pos_neg_diff, score_average_rating ve wilson_lower_bound Skorlarını Hesaplayıp Veriye Ekleyiniz
-###################################################
 
 def wilson_lower_bound(up, down, confidence=0.95):
     """
@@ -153,8 +99,7 @@ df["score_average_rating"] = df.apply(lambda x: score_average_rating(x["helpful_
 df["wilson_lower_bound"] = df.apply(lambda x: wilson_lower_bound(x["helpful_yes"], x["helpful_no"]), axis=1)
 
 ##################################################
-# Adım 3. 20 Yorumu Belirleyiniz ve Sonuçları Yorumlayınız.
-###################################################
+
 
 df.sort_values("wilson_lower_bound", ascending=False).head(20)
 
